b19bbe72e03cffb1d631721c01229b2cacc55555
AJ <aj.cr7.123@gmail.com>
2023-08-23 15:27:03 -0400

    reviewed and edited minor bugs in piece class

--- piece.py
+++ piece.py
@@ -1,113 +1,106 @@
 """
-This file is for the pieces
+Chess piece implementations
 """
 from board import Board
 
-class Piece():
-  
-  def __init__(self, color, pos, piece_type):
-    """_summary_
+
+class Piece:
+  def __init__(self, color, pos):
+    """Initialize piece 
 
     Args:
         color (str): 'B' or 'W' for black and white
-        pos (List[int]): a list of length 2 containing row and col
-        piece_type (str): str of piece
-  
+        pos (List[int]): A 2-element list of [row, col]
+        piece_type (str): String representation of piece by FEN 
     """
     self.color = color
     self.pos = pos
-    self.piece_type = piece_type
-    self.legal_moves = self.get_legal_moves()
+    self.piece_type = None
 
-  
   def __str__(self):
-    raise NotImplementedError("")
-  # def calculate_moves(self, board, ):
-     
+    if self.piece_type == None:
+      raise NotImplementedError("This piece is not yet defined")
+    return self.piece_type
 
   def set_pos(self, new_pos):
-    """_summary_
-
-    Args:
-        new_pos (List[int]): _description_
-    """
     self.pos = new_pos
 
-
   def remove_pos(self):
-      self.pos = None
+    self.pos = None
 
   def get_pos(self):
-      return self.pos
+    return self.pos
 
   def get_color(self):
-     return self.color
-  
+    return self.color
+
   def get_legal_moves(self, board):
-    """_summary_
+    """Return a list of legal piece moves
 
     Args:
-        board (Board): _description_
+        board (Board): Board object
 
     Raises:
-        NotImplementedError: _description_
+        NotImplementedError: Method should be overrided by children classes
     """
     raise NotImplementedError
+
 
 class King(Piece):
   def __init__(self):
     super().__init__()
-  
-  
+    self.piece_type = "K" if self.color == "W" else "k"
+
+
 class Knight(Piece):
 
-  MOVE_OFFSETS  = [(1, 2), (1, -2), (-1, 2), (-1, -2), (2, 1), (2, -1), (-2, 1), (-2, -1)]
+  MOVE_OFFSETS = [(1, 2), (1, -2), (-1, 2), (-1, -2),
+                  (2, 1), (2, -1), (-2, 1), (-2, -1)]
+
   def __init__(self):
-     super().__init__()
-  
+    super().__init__()
+    self.piece_type = "N" if self.color == "W" else "n"
+
   def get_legal_moves(self, board):
-    """_summary_
+    """Return a list of legal piece moves
 
     Args:
-        board (Board): _description_
+        board (Board): Board object
     """
 
     curr_pos = self.pos
-
     legal_moves = []
 
     for offset in Knight.MOVE_OFFSETS:
-       possible_pos = [curr_pos[0] + offset[0], curr_pos[1] + offset[1]]
+      possible_pos = [curr_pos[0] + offset[0], curr_pos[1] + offset[1]]
 
-       if not board.is_valid_location(possible_pos):
-          continue
-       elif board.get_cell_piece() is not None:
-          continue
-       elif board.get_cell_piece().get_color() == self.get_color():
-          continue
-       else:
-          legal_moves.add(possible_pos)
-  
-        
+      if not board.is_valid_location(possible_pos):
+        continue
+      elif board.get_cell_piece(possible_pos).get_color() == self.get_color():
+        continue
+      else:
+        legal_moves.add(possible_pos)
 
 
-       
-       
-
-
-    
-  
 class Pawn(Piece):
   def __init__(self):
-     return 
+    super().__init__()
+    self.piece_type = "P" if self.color == "W" else "p"
+
 
 class Queen(Piece):
+  def __init__(self):
+    super().__init__()
+    self.piece_type = "Q" if self.color == "W" else "q"
+
 
 class Rook(Piece):
   def __init__(self):
-     return 
+    super().__init__()
+    self.piece_type = "R" if self.color == "W" else "r"
+
 
 class Bishop(Piece):
   def __init__(self):
-     return 
-  
+    super().__init__()
+    self.piece_type = "B" if self.color == "w" else "b"
