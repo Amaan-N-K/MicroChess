e478b7170f561050578a25f09d9bcc68ac86be45
Amaan-N-K <amaan.n.khan.0102@gmail.com>
2023-08-24 16:09:21 -0400

    Updated game class

--- game.py
+++ game.py
@@ -14,17 +14,47 @@ class Game:
     self.board.get_cell_piece(curr_pos)
     self.board.move_piece(curr_pos, new_pos)    
   
-  def is_game_over(self):
-    
+  def is_game_over(self, color):
+    if color == "B":
+      self.board.w_king.checks_and_pins()
+      all_moves = set()
+      for piece in self.board.get_all_pieces_by_color("W"):
+        all_moves.update(piece.get_legal_moves(self.board))
+        if len(all_moves) == 0 and len(self.board.w_king.get_checks()) > 1:
+          return -1
+        elif len(all_moves) == 0:
+          return 0
+    else:
+      self.board.b_king.checks_and_pins()
+      all_moves = set()
+      for piece in self.board.get_all_pieces_by_color("B"):
+        all_moves.update(piece.get_legal_moves(self.board))
+        if len(all_moves) == 0 and len(self.board.b_king.get_checks()) > 1:
+          return "W"
+        elif len(all_moves) == 0:
+          return 1
+
 
   def play(self):
-    while not self.is_game_over():
+    state = "playing"
+    game_over = False
+    while not game_over:
       for player in [self.p1, self.p2]:
         curr_pos, new_pos = player.get_move(self.board)
         self.apply_move(curr_pos, new_pos)
-        if self.is_game_over(): 
+
+        state = self.is_game_over(player.color)
+        if state is int:  
+          game_over = True
           break
 
+    if state == -1:
+      return "Black Wins!"
+    elif state == 1:
+      return "White Wins!"
+    else:
+      return "Draw"
+    
 
 class Agent:
   def __init__(self, board: Board, color):
