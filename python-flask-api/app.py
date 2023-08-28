b2d9bf93364808eb16f797e8899409e064626bd1
AJ <aj.cr7.123@gmail.com>
2023-08-28 18:19:05 -0400

    Setup the flask and frontend movement to a rudimentry level

--- python-flask-api/app.py
+++ python-flask-api/app.py
@@ -1,6 +1,19 @@
-import json
-from flask import Flask, jsonify, request
-app = Flask(__name__)
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
-if __name__ == '__main__':
-  app.run(port=5000)
+app = Flask(__name__)
+CORS(app, origins=["http://127.0.0.1:5500"])
+
+
+@app.route('/move', methods=['POST'])
+def move():
+  data = request.json
+  old_coor = data.get('old_coor')
+  new_coor = data.get('new_coor')
+  print(f"Moving piece from {old_coor} to {new_coor}")
+
+  return jsonify({"message": "Moved successfully"}), 200
+
+
+if __name__ == "__main__":
+  app.run(debug=True)
--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 const board = document.querySelector(".board");
 
-function handleCellClick(event) {
-  alert(
-    `You clicked on row ${event.target.dataset.row_count}, column ${event.target.dataset.col_count}`
-  );
-}
-
 const initialSetup = [
   ["king-b", "knight-b", "bishop-b", "rook-b"],
   ["pawn-b"],
@@ -35,4 +29,54 @@ for (let i = 0; i < 5; i++) {
     row.appendChild(cell);
   }
   board.appendChild(row);
+}
+
+let selectedCell = null;
+
+async function handleCellClick(event) {
+  const row = event.target.dataset.row_count;
+  const col = event.target.dataset.col_count;
+  console.log("Event target:", event.target);
+  console.log("Classes:", event.target.classList);
+
+  if (!selectedCell) {
+    selectedCell = { row, col };
+  } else {
+    // Send request to Flask API
+    const response = await fetch("http://localhost:5000/move", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        old_coor: selectedCell,
+        new_coor: { row, col },
+      }),
+    });
+
+    if (response.ok) {
+      // Remove classes from the old cell
+      const oldCell = document.querySelector(
+        `[data-row_count='${selectedCell.row}'][data-col_count='${selectedCell.col}']`
+      );
+      const newCell = document.querySelector(`[data-row_count='${row}'][data-col_count='${col}']`);
+
+      const pieceClass = Array.from(oldCell.classList).find(
+        (cls) => cls.endsWith("-b") || cls.endsWith("-w")
+      );
+
+      if (pieceClass) {
+        // Remove the class representing the piece from the old cell
+        oldCell.classList.remove(pieceClass);
+
+        // Add the class to the new cell
+        event.target.classList.add(pieceClass);
+      }
+      oldCell.classList.remove("chess-piece");
+      newCell.classList.remove("chess-piece");
+      newCell.classList.add("chess-piece");
+    }
+
+    selectedCell = null;
+  }
 }
