79dfd116c2b3ca9b00e2e8bc10b1d6fdc6c45b3b
AJ <aj.cr7.123@gmail.com>
2023-08-29 22:00:05 -0400

    fixed some bugs

--- agent.py
+++ agent.py
@@ -5,14 +5,21 @@ class Agent:
   def __init__(self, color: int, board: Board) -> None:
     self.color = color
     self.board = board
+    self.info = None
 
   def get_color(self) -> int:
     return self.color
 
+  def get_info(self) -> any:
+    return self.info
+
+  def set_info(self, info: any) -> None:
+    self.info = info
+
   def my_king(self) -> any:
     return self.board.get_piece("K") if self.get_color() == 0 else self.board.get_piece("k")
 
-  def get_move(self) -> list[tuple[int, int]]:
+  def get_move(self) -> list[int, tuple[int, int]]:
     raise NotImplementedError("This method must be ovverided by child classes")
 
 
@@ -20,32 +27,23 @@ class HumanAgent(Agent):
   def __init__(self, color: int, board: Board) -> None:
     super().__init__(color, board)
 
-  def get_move(self) -> list[tuple[int, int]]:
+  def get_move(self) -> list[int, tuple[int, int]]:
     count = 0
-    print("###################################")
     self.board.print_board()
+    if self.get_info():
+      print(self.get_info())
     while True:
       try:
-        curr_pos = input(
-            "Enter the [row col] of the piece to move: ").split(" ")
-        curr_pos[0] = int(curr_pos[0])
-        curr_pos[1] = int(curr_pos[1])
-        curr_pos = tuple(curr_pos)
-        if not self.board.is_valid_pos(curr_pos):
+        choice = input("Enter [OPTION] [row] [col] [row] [col]: ").split(" ")
+        option = int(choice[0])
+        curr_pos = (int(choice[1]), int(choice[2]))
+        new_pos = (int(choice[3]), int(choice[4]))
+
+        if not self.board.is_valid_pos(curr_pos) or not self.board.is_valid_pos(new_pos):
+          print("Invalid choice -- try again")
           continue
 
-        piece = self.board.lookup(curr_pos)
-        print(f"You chose to move: {str(piece)}")
-
-        new_pos = input("Enter the [row col] to move the piece: ").split(" ")
-        new_pos[0] = int(new_pos[0])
-        new_pos[1] = int(new_pos[1])
-        new_pos = tuple(new_pos)
-
-        if not self.board.is_valid_pos(new_pos):
-          continue
-
-        return [curr_pos, new_pos]
+        return [option, curr_pos, new_pos]
 
       except:
         print("Invalid choice -- try again")
--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 from typing import *
 from board import Board
 from piece import Piece, King, Knight, Pawn, Queen, Rook, Bishop
-from agent import HumanAgent
+from agent import *
 WHITE, BLACK = 0, 1
 ROW_SIZE = 5
 COL_SIZE = 4
@@ -17,17 +17,53 @@ class Game:
   def play(self) -> None:
     while True:
       for p in self.players:
+        p.my_king()[0].checks_and_pins()
         game_state = self.is_game_over(p.color)
         if game_state[0]:
           return game_state[1]
-        curr_pos, new_pos = p.get_move()
-        piece = self.board.lookup(curr_pos)
-        if piece.get_color() != p.get_color() or new_pos not in piece.moves():
-          print("Invalid choice -- you lose a turn HAHAHA")
+        print("###################################")
+        print(
+            f"Color to move: {'white' if p.get_color() == WHITE else 'black'}")
+        print(
+            f"BEFORE My king is checked by: {p.my_king()[0].get_checks()} and pinned by {p.my_king()[0].get_pins()}")
+        while self.move(p) != "DONE":
           continue
-        self.board.remove(curr_pos)
+
+        print(
+            f"AFTER My king is checked by: {p.my_king()[0].get_checks()} and pinned by {p.my_king()[0].get_pins()}")
+        print("###################################")
+
+  def move(self, p: Agent) -> None:
+    option, curr_pos, new_pos = p.get_move()
+    curr_pos_piece = self.board.lookup(curr_pos)
+    new_pos_piece = self.board.lookup(new_pos)
+
+    # Get all piece moves given piece coordinates
+    if option == 0:
+      moves = curr_pos_piece.moves()
+      p.set_info(moves)
+      return
+
+    # Move a piece to a new coordinate
+    elif option == 1:
+      if curr_pos_piece.get_color() != p.get_color() or new_pos not in curr_pos_piece.moves():
+        p.set_info("Invalid choice -- try again")
+        return
+
+      self.board.remove(curr_pos)
+      self.board.forget_piece(curr_pos_piece)
+      curr_pos_piece.remove_pos()
+
+      if new_pos_piece != None:
         self.board.remove(new_pos)
-        self.board.place(new_pos, piece)
+        self.board.forget_piece(new_pos_piece)
+        new_pos_piece.remove_pos()
+
+      self.board.place(new_pos, curr_pos_piece)
+      self.board.add_piece(curr_pos_piece)
+      curr_pos_piece.set_pos(new_pos)
+
+      return "DONE"
 
   def make_piece(self, fen_char: str, pos: tuple[int, int]) -> Piece:
     mapping = {
@@ -61,6 +97,7 @@ class Game:
           piece = self.make_piece(c, (row_idx, col_idx))
           self.board.place((row_idx, col_idx), piece)
           self.board.add_piece(piece)
+          piece.set_pos((row_idx, col_idx))
           col_idx += 1
 
   def is_game_over(self, color) -> list:
@@ -92,12 +129,3 @@ class Game:
           all_moves.extend(piece.moves())
 
     return all_moves
-
-# test
-
-
-
-
-
-
-
